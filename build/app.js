a85b229defb17ac5eab108a0409763ee65ed8a6b
Sxm <sxm@gmail.com>
2024-04-30 17:09:44 -0500

    changes

---
+++ build/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.get("/", (req, res) => {
+    res.send("Hellfsafa World");
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port} `);
+});
--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("Hellfsafa World");
+app.get("/", (_req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
